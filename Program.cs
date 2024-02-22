8cb5cf4aa9d7d5f74a6c1b8a266e40a14b8e5775
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:53:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+impar
+ In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+ Google :even and odd number*/
+
+
+Console.WriteLine("Introduce un numar: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Numarul este par.");
+}
+
+else
+{
+    Console.WriteLine("Nuarul este impar. ");
+}
+Console.ReadLine();
